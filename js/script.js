35a73c92fdd8f98cce70d804d2417728235a6f22
jesteenajose123 <jesteenaj19@gmail.com>
2024-08-06 22:56:12 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,4 +15,41 @@ $(document).ready(function() {
       $('.close').on('click', function() {
         $('.side-bar').removeClass('visible');
       });
+
+
+      
+$('.testimonial-cards').slick({
+  
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    
+  ]
 });
+});
+
+
